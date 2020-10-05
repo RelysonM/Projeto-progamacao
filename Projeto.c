9d65330ab45057a22c777cf36c27e62168d77585
Relyson Medeiros <relyson.m@gmail.com>
2020-10-05 19:08:53 -0300

    telaExemplo

---
+++ Projeto.c
@@ -0,0 +1,52 @@
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                 Developed by @RelysonM - Sep, 2020                      ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                       Projeto cigana virtual                            ///
+///                Developed by  @RelysonM - Sep, 2020                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+	menuSobre();
+    printf("          ----Bem vindo a SIG-Aninha----          \n");
+    printf("            ----sua cigana virtual----            \n");
+    printf("                 ##############\n");
+    printf("                 ##   Menu   ##\n");
+    printf("                 ##############\n");
+    printf("1 - Cadastrar usuário\n");
+    printf("1 - Login\n");
+    printf("1 - Horóscopo\n");
+    printf("1 - Tarot virtual\n");
+	return 0;
+}
+
+
+void menuSobre(void) {
+	printf("\n");
+	printf("///////////////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                                 ///\n");
+	printf("///                 Universidade Federal do Rio Grande do Norte                     ///\n");
+	printf("///                     Centro de Ensino Superior do Seridó                         ///\n");
+	printf("///                   Departamento de Computação e Tecnologia                       ///\n");
+	printf("///                      Disciplina DCT1106 -- Programação                          ///\n");
+	printf("///                           Projeto cigana virtual                                ///\n");
+	printf("///                     Developed by  @RelysonM - Sep, 2020                         ///\n");
+	printf("///                                                                                 ///\n");
+	printf("///////////////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                                 ///\n");
+	printf("///                      = = = = = Cigana virtual = = = = =                         ///\n");
+	printf("///                                                                                 ///\n");
+	printf("///  Programa prático utilizado na disciplina DCT1106 - Programação, para fins      ///\n");
+	printf("///  avaliativos e de aprendizado. O programa tem como objetivo apresentar          ///\n");                                           ///\n");
+	printf("///  adivinhações com base na astrologia e outras formas de divinação contendo      ///\n");
+	printf("///  os principais módulos e funções exigidas para avaliação. Serve como avaliação  ///\n");
+	printf("///  do conhecimento adquirido durante todo o decorrer da disciplina.               ///\n");
+	printf("///                                                                                 ///\n");
+	printf("///////////////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+}

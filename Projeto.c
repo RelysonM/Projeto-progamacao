8359f1cf54e2f6c37131e362049160e2ad7246e1
Relyson Medeiros <relyson.m@gmail.com>
2020-10-09 21:26:06 -0300

    T

--- Projeto.c
+++ Projeto.c
@@ -1,52 +1,41 @@
-///             Universidade Federal do Rio Grande do Norte                 ///
-///                 Centro de Ensino Superior do Seridó                     ///
-///               Departamento de Computação e Tecnologia                   ///
-///                 Developed by @RelysonM - Sep, 2020                      ///
-///                  Disciplina DCT1106 -- Programação                      ///
-///                       Projeto cigana virtual                            ///
-///                Developed by  @RelysonM - Sep, 2020                    ///
-///////////////////////////////////////////////////////////////////////////////
-
 #include <stdio.h>
 
-void menuSobre(void);
+void menuCadastro(void);
+void menuFuncao(void);
+
 
 int main(void) {
-	menuSobre();
-    printf("          ----Bem vindo a SIG-Aninha----          \n");
-    printf("            ----sua cigana virtual----            \n");
-    printf("                 ##############\n");
-    printf("                 ##   Menu   ##\n");
-    printf("                 ##############\n");
-    printf("1 - Cadastrar usuário\n");
-    printf("1 - Login\n");
-    printf("1 - Horóscopo\n");
-    printf("1 - Tarot virtual\n");
+    printf("================================\n");
+	printf("==== Bem vindo à SIG-Aninha ====\n");
+    printf("====== sua cigana virtual ======\n");
+	printf("================================\n");
+    menuCadastro ();
+    menuFuncao ();
 	return 0;
 }
 
 
-void menuSobre(void) {
+void menuCadastro(void) {
+	printf("################################\n");
+    printf("##            Menu            ##\n");
+    printf("################################\n");
+    printf("##  1 - Cadastrar usuário     ##\n");  
+	printf("##  2 - Alterar cadastro      ##\n");
+	printf("##  3 - Excluir usuário       ##\n");
+	printf("##  4 - Login                 ##\n");
+	printf("##  0 - Sair                  ##\n");
+	printf("################################\n");
 	printf("\n");
-	printf("///////////////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                                 ///\n");
-	printf("///                 Universidade Federal do Rio Grande do Norte                     ///\n");
-	printf("///                     Centro de Ensino Superior do Seridó                         ///\n");
-	printf("///                   Departamento de Computação e Tecnologia                       ///\n");
-	printf("///                      Disciplina DCT1106 -- Programação                          ///\n");
-	printf("///                           Projeto cigana virtual                                ///\n");
-	printf("///                     Developed by  @RelysonM - Sep, 2020                         ///\n");
-	printf("///                                                                                 ///\n");
-	printf("///////////////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                                 ///\n");
-	printf("///                      = = = = = Cigana virtual = = = = =                         ///\n");
-	printf("///                                                                                 ///\n");
-	printf("///  Programa prático utilizado na disciplina DCT1106 - Programação, para fins      ///\n");
-	printf("///  avaliativos e de aprendizado. O programa tem como objetivo apresentar          ///\n");                                           ///\n");
-	printf("///  adivinhações com base na astrologia e outras formas de divinação contendo      ///\n");
-	printf("///  os principais módulos e funções exigidas para avaliação. Serve como avaliação  ///\n");
-	printf("///  do conhecimento adquirido durante todo o decorrer da disciplina.               ///\n");
-	printf("///                                                                                 ///\n");
-	printf("///////////////////////////////////////////////////////////////////////////////////////\n");
+}
+
+void menuFuncao(void) {
+	printf("################################\n");
+	printf("##       Bem vind@ nome       ##\n");
+	printf("################################\n");
+	printf("##  1 - Horóscopo semanal     ##\n");
+	printf("##  2 - Mais sobre seu sígno  ##\n");
+	printf("##  3 - Tarot virtual         ##\n");       
+	printf("##  0 - Alterar usuário       ##\n");
+	printf("################################\n");
 	printf("\n");
 }
